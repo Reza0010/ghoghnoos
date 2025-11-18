e688df3dd3c107cbb295da7eb0244953d55f24d2
Reza0010 <161936904+Reza0010@users.noreply.github.com>
2025-11-18 23:32:20 +0330

    Delete components directory

--- components/PromptChainEditor.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// This component is no longer in use. The functionality has been moved to PromptChainStudio.tsx
-// as a specific, guided workflow instead of a generic chain editor.
-const PromptChainEditor: React.FC = () => {
-  return null;
-};
-
-export default PromptChainEditor;
